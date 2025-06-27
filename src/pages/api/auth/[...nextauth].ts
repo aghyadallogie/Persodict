2561072187984a8123601c455239be666d396f79
Aghy <zenlogie@gmail.com>
2025-06-27 23:24:17 +0200

    fix(nextauth): switch bcrypt to bcryptjs library

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-// import { prisma } from "@/server/utils/prisma";
-// import bcrypt from "bcrypt";
+import { prisma } from "@/server/utils/prisma";
+import bcrypt from "bcryptjs";
 import NextAuth, { NextAuthOptions } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -22,37 +22,37 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     email: { label: "Email", type: "email" },
-    //     password: { label: "Password", type: "password" },
-    //   },
-    //   async authorize(credentials) {
-    //     const { email, password } = credentials ?? {};
-    //     if (!email || !password) throw new Error("Missing credentials");
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials ?? {};
+        if (!email || !password) throw new Error("Missing credentials");
 
-    //     let user = await prisma.user.findUnique({ where: { email } });
+        let user = await prisma.user.findUnique({ where: { email } });
 
-    //     if (!user) {
-    //       user = await prisma.user.create({
-    //         data: {
-    //           email,
-    //           name: 'unnamed',
-    //           hashedPassword: await bcrypt.hash(password, 10)
-    //         }
-    //       });
-    //       return { id: user.id, email: user.email };
-    //     }
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              email,
+              name: "unnamed",
+              hashedPassword: await bcrypt.hash(password, 10)
+            }
+          });
+          return { id: user.id, email: user.email };
+        }
 
-    //     if (!user.hashedPassword) throw new Error("Invalid credentials");
+        if (!user.hashedPassword) throw new Error("Invalid credentials");
 
-    //     const isValid = await bcrypt.compare(password, user.hashedPassword);
-    //     if (!isValid) throw new Error("Invalid credentials");
+        const isValid = await bcrypt.compare(password, user.hashedPassword);
+        if (!isValid) throw new Error("Invalid credentials");
 
-    //     return { id: user.id, email: user.email };
-    //   },
-    // }),
+        return { id: user.id, email: user.email, name: user.name ?? "unnamed" };
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
